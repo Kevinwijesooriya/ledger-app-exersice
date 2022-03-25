6af634d817571f076f6c6a4ed49bebb5e836b5a5
kevinwijesooriya <kevinwijesooriya@gmail.com>
2022-03-25 11:43:09 +0530

    added payment class getters

--- model/payment.ts
+++ model/payment.ts
@@ -1,12 +1,18 @@
 class Payment {
-  lumpSum : number;
-  emiNum : number;
+  lumpSum: number;
+  emiNum: number;
 
   constructor(lumpSum, emiNum) {
     this.lumpSum = lumpSum;
     this.emiNum = emiNum;
   }
 
-  
+  get getLumpSum() {
+    return this.lumpSum;
+  }
+
+  get getEmiNum() {
+    return this.emiNum;
+  }
 }
 export { Payment };
