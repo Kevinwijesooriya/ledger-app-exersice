8d129935e2c63620a063f86b57491c95b589ce83
kevinwijesooriya <kevinwijesooriya@gmail.com>
2022-03-25 11:38:34 +0530

    added payment class constructor

---
+++ model/payment.ts
@@ -0,0 +1,12 @@
+class Payment {
+  lumpSum : number;
+  emiNum : number;
+
+  constructor(lumpSum, emiNum) {
+    this.lumpSum = lumpSum;
+    this.emiNum = emiNum;
+  }
+
+  
+}
+export { Payment };
